367e6b9e9449dc2e785a03f067412f0a6e311fe4
Pralhad <mrpatilpralhad@gmail.com>
2017-08-20 13:20:45 +0530

    Basic layout has been made

--- app.js
+++ app.js
@@ -43,4 +43,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(8080);
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var model = require("../module/model");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let itemsList = model.getItems();
+  
+  res.render('index', { title: 'Inventory System',itemsList:itemsList.items });
 });
 
 module.exports = router;
